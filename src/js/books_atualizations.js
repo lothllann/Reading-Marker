10455a206b897a98051daa0970a50a4b10262726
lothllann <nick.ruppenthal@gmail.com>
2022-02-09 16:41:41 -0300

    botao 'atualize suas leituras'

--- src/js/books_atualizations.js
+++ src/js/books_atualizations.js
@@ -1,5 +1,16 @@
 const Atualizations = () => {
 
+    const btnIniciar = document.querySelector('[data-btn-atualizar]');
+    const containerAtualizar = document.querySelector('[data-container-atualizar]');
+
+    function ativarContainer(event) {
+        event.preventDefault();
+        containerAtualizar.classList.toggle('ativo')
+        btnIniciar.classList.toggle('ativo')
+    }
+
+
+    btnIniciar.addEventListener('click', ativarContainer)
 }
 
 export default Atualizations;
