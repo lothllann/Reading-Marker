36d69802ded6cdb09ae669ab8462916314c3c964
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-03-15 17:34:13 -0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import Informations from './books_informations.js'
-import Atualizations from './books_atualizations.js'
-import Menu from './menu.js'
-
-
-Informations();
-Atualizations();
-Menu();
