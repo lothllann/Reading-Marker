79346e5a023ab3e357454f56b1ff3bf05e84badd
Nickolas <89612417+lothllann@users.noreply.github.com>
2022-03-15 17:23:44 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import Informations from './books_informations.js'
+import Atualizations from './books_atualizations.js'
+import Menu from './menu.js'
+
+
+Informations();
+Atualizations();
+Menu();
